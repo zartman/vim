71381a1351aba37964bc5ae1f4dba21df1944de1
jzartman <jzartman@aegistg.com>
2019-08-05 16:13:00 -0500

    Added verilog boilerplate. Added swap file directory.

---
+++ verilog_boilerplate.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+/*******************************************************************************
+Company        : AEgis Technologies
+Engineer       : John Zartman
+
+Create Date    :
+Design Name    :
+Module Name    : MODULE_NAME
+Project Name   :
+Target Devices : Zedboard
+Tool Versions  : Vivado 2019.1
+
+Description:
+
+Dependencies:
+
+Additional Comments:
+
+*******************************************************************************/
+
+module MODULE_NAME #(
+  parameter
+) (
+  input wire
+  output reg
+);
+
+endmodule
+
